1be6380888a8ffa4b7e2dec1e6e013a1782f4678
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-19 14:47:47 -0500

    fleshed out the product class

--- product.py
+++ product.py
@@ -2,11 +2,19 @@
 # total price based on the base price and tax rate.
 
 class Product:
+    """
+    This class represents a retail product. It has a name, a base price, and a tax rate
+    """
 
     def __init__(self,name,price,tax=0):
         self.name = name
         self.price = float(price)
         self.tax = float(tax)
 
+    #str function to describe the product itself
     def __str__(self):
         return "The product is {}, it costs ${:.2f}, and has a tax rate of {}.".format(self.name,self.price,self.tax)
+
+    #Function to figure out the total cost with taxes
+    def total_price(self):
+        return self.price * (1 + self.tax/100)
