719f4eab18350cd0ae7423c1e028f7221c31c1a3
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-19 15:11:05 -0500

    met the base requirements of the assignment

--- cart.py
+++ cart.py
@@ -8,44 +8,53 @@
 import product
 
 class Cart:
+    """
+    This class represents a shopping cart. It only consists of a list of products, but it has other useful methods as well
+    """
 
+    #General init function. You can either create a cart with an existing list, or you can create an empty cart
     def __init__(self,items=[]):
         self.items = items
 
-    def add_item(self,product):
-        self.items.append(product)
-
-    def remove_item(self,product):
-        self.items.remove(product)
-
+    #String method to describe the list of items in the cart
     def __str__(self):
         list_of_items = "The following items are in your cart:\n"
         for num in range(0,len(self.items)):
             list_of_items += "{}\n".format(self.items[num])
         return list_of_items
 
+    #Method to add a product to the cart
+    def add_item(self,product):
+        self.items.append(product)
+
+    #Method to remove a product from the cart
+    def remove_item(self,product):
+        self.items.remove(product)
+
+    #Method to return the sum of the base costs of each item in the cart
     def total_before_tax(self):
         total = 0
         for num in range(0,len(self.items)):
             total += self.items[num].price
         return str(total)
 
+    #Method to return the sum of the total costs of each item in the cart with taxes applied
     def total_after_tax(self):
         total = 0
         for num in range(0,len(self.items)):
             total += self.items[num].total_price()
         return str(total)
 
-macbook = product.Product("macbook",2000,5)
-water_bottle = product.Product("water bottle",3,10)
-pants = product.Product("pants",50,5)
-
-
-cart1 = Cart([macbook,water_bottle])
-print(cart1)
-cart1.add_item(pants)
-print(cart1)
-cart1.remove_item(macbook)
-print(cart1)
-print(cart1.total_before_tax())
-print(cart1.total_after_tax())
+# macbook = product.Product("macbook",2000,5)
+# water_bottle = product.Product("water bottle",3,10)
+# pants = product.Product("pants",50,5)
+#
+#
+# cart1 = Cart([macbook,water_bottle])
+# print(cart1)
+# cart1.add_item(pants)
+# print(cart1)
+# cart1.remove_item(macbook)
+# print(cart1)
+# print(cart1.total_before_tax())
+# print(cart1.total_after_tax())
